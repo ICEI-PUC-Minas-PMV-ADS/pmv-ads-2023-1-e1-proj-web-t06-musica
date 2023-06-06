92a78dd01ed39f219778801b5b85599ba02f207b
Marcos Kutova <kutova@gmail.com>
2023-06-06 20:04:31 -0300

    Update home.js

--- src/js/home.js
+++ src/js/home.js
@@ -16,3 +16,36 @@ modalClose.click(function () {
 //modalBackground.click(function () {
 //    modal.fadeOut(500);
 //});
+
+
+// Recupera o banco de dados
+let artistas = JSON.parse( localStorage.getItem('artistas') ) || [];
+
+// Cria o método para a busca, ao clicar no botão
+document.querySelector('#btnPesquisar').onclick = function(e) {
+    // Recupera o valor do campo (e cancela se estiver vazio)
+    let campoPesquisa = document.querySelector('#inputPesquisa');
+    if(campoPesquisa.value='')
+        return;
+    
+    // Identifica o campo e cria um conteúdo vazio inicial
+    let listaArtistas = document.querySelector('#listaArtistas');
+    let listaArtistas.innerHTML = '<ul>';
+    
+    // Inclui os artistas pesquisados
+    for(let i in artistas) {
+        let art = artistas[i];
+        listaArtistas.innerHTML += `
+             <li>
+                 <strong>${art.nome}</strong><br />
+                 Estilo: ${art.estilo}<br />
+                 Região: ${art.regiao}<br />
+                 Observação: ${art.observacao}<br />
+             </li>
+        `;
+    }
+    
+    // Conclui o conteúdo da lista
+    listaArtistas.innerHTML += '</ul>';
+    
+}
